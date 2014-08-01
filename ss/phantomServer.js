213aa19446e73f7986d080e9ab281549f82e6d4e
Andy Gray <andyg@cakesolutions.net>
2014-08-01 19:42:45 +0100

    scrape_pga

--- ss/phantomServer.js
+++ ss/phantomServer.js
@@ -1,31 +1,28 @@
 var phantom = require('phantom');
+
+var url = "http://www.worldgolfchampionships.com/bridgestone-invitational/leaderboard.html";
 phantom.create(function(ph) {
     return ph.createPage(function(page) {
-        return page.open("http://tilomitra.com/repository/screenscrape/ajax.html", function(status) {
-            console.log("opened site? ", status);
+        console.log("opening site: ", url);
+        return page.open(url, function(status) {
+            console.log("opened site: ", status);
 
             page.injectJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
-                //jQuery Loaded.
                 //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
                 setTimeout(function() {
                     return page.evaluate(function() {
 
-                        //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
-                        var h2Arr = [],
-                            pArr = [];
-                        $('h2').each(function() {
-                            h2Arr.push($(this).html());
+                        var playerAndTotal = []
+                        $('.row-main').each(function() {
+                            var pos = $.trim($(this).find('.col-pos').html());
+                            var total = $.trim($(this).find('.col-total').html());  
+                            var player = $.trim($(this).find('.col-player div .expansion').html());  
+                            playerAndTotal.push({pos: pos, player: player, total: total});         
                         });
-                        $('p').each(function() {
-                            pArr.push($(this).html());
-                        });
-
-                        return {
-                            h2: h2Arr,
-                            p: pArr
-                        };
+             
+                        return playerAndTotal;
                     }, function(result) {
-                        console.log(result);
+                         console.log(result);
                         ph.exit();
                     });
                 }, 5000);
