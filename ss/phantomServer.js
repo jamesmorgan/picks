67f44a86f93f732b08943305bb2679dab4329df6
Andy Gray <andyg@cakesolutions.net>
2014-08-04 17:24:23 +0100

    comments_for_review

--- ss/phantomServer.js
+++ ss/phantomServer.js
@@ -2,31 +2,15 @@ var phantom = require('phantom');
 var rest = require('restler');
 var repeat = require('repeat');
 
-var golf = {
-    url: "http://www.worldgolfchampionships.com/bridgestone-invitational/leaderboard.html",
-    gameid:"53de3289e4b00257865775cd",
-    scrape: function (pg) {
-        var playerAndTotal = []
-        $('.row-main').each(function() {
-            var pos = $.trim($(this).find('.col-pos').html());
-            var score = $.trim($(this).find('.col-total').html());
-            var player = $.trim($(this).find('.col-player div .expansion').html());
-            playerAndTotal.push({
-                pos: pos,
-                player: player,
-                score: score
-            });
-        });
-
-        return playerAndTotal;
-    }
-}
-
+// should be part of seed params ideally passed to phantom
 var url = "http://www.worldgolfchampionships.com/bridgestone-invitational/leaderboard.html";
+
 var phantomFunc = function() {
+    // should we create a new phantom browser on each run?
     phantom.create(function(ph) {
         return ph.createPage(function(page) {
             console.log("opening site: ", url);
+            
             return page.open(url, function(status) {
                 console.log("opened site: ", status);
 
@@ -35,6 +19,7 @@ var phantomFunc = function() {
                     setTimeout(function() {
                         return page.evaluate(function() {
 
+                            // this logic needs extracting and plugged in so can be used for multiple sites/
                             var playerAndTotal = []
                             $('.row-main').each(function() {
                                 var pos = $.trim($(this).find('.col-pos').html());
@@ -49,13 +34,16 @@ var phantomFunc = function() {
 
                             return playerAndTotal;
                         }, function(result) {
+
+                            // extract to a find and update service based on the result array
                             console.log('found ' + result.length + ' players');
                             result.forEach(function(player) {
-                                rest.get('http://localhost:3000/selections/53de3289e4b00257865775cd/find/' + player.player)
+                                console.log(player.player);
+                                rest.get('http://picks.whatsthescore.webfactional.com/selections/53de3289e4b00257865775cd/find/' + player.player)
                                     .on('complete', function(data) {
                                         if (data && data._id) {
                                             console.log('found ' + player.player + ' > ' + data._id + ' : ' + player.score);
-                                            return rest.post('http://localhost:3000/selections/53de3289e4b00257865775cd/update', {
+                                            return rest.post('http://picks.whatsthescore.webfactional.com/selections/53de3289e4b00257865775cd/update', {
                                                 data: {
                                                     adminPass: 'Bird',
                                                     _id: data._id,
@@ -79,9 +67,9 @@ var phantomFunc = function() {
 };
 
 repeat(phantomFunc).every(1, 'min')
-  .for(3, 'min')
-  .start.now()
-  .then(function() {
-    console.log("Completed");
-    process.exit(code=0);
-  });
+    .for(3, 'min')
+    .start.now()
+    .then(function() {
+        console.log("Completed");
+        process.exit(code = 0);
+    });
